3e4e52f054b54b18e2ff53a0cf4f30612c77ba8e
wesleyambrozio <wesleyromano@yahoo.com.br>
2026-08-31 13:32:52 -0300

    fix: torna migracao de suporte idempotente

--- database/migrations/013_support_integrity_mysql.sql
+++ database/migrations/013_support_integrity_mysql.sql
@@ -1,7 +1,3 @@
 alter table knowledge_documents engine=InnoDB;
 alter table system_users engine=InnoDB;
 alter table support_ticket_messages engine=InnoDB;
-alter table knowledge_documents add constraint knowledge_documents_system_fk foreign key(system_id) references saas_systems(id);
-alter table knowledge_documents add constraint knowledge_documents_author_fk foreign key(created_by) references admin_users(id);
-alter table system_users add constraint system_users_system_fk foreign key(system_id) references saas_systems(id);
-alter table support_ticket_messages add constraint support_ticket_messages_ticket_fk foreign key(ticket_id) references support_tickets(id);
